9de37016dabcececc310037fa16cc9d82e6c29ef
zhangkun <245101751@qq.com>
2017-05-20 23:13:36 +0800

    push

--- dubbo_consumer/src/main/java/com/java/zhangkungit/Consumer.java
+++ dubbo_consumer/src/main/java/com/java/zhangkungit/Consumer.java
@@ -8,10 +8,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Consumer {
 
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"dubbo-demo-consumer.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"dubbo-demo-consumer.xml"});
         context.start();
-        DemoService demoService = (DemoService)context.getBean("demoService"); // get service invocation proxy
-        String hello = demoService.sayHello("world");
-        System.out.println( hello );
+
+        // get service invocation proxy
+        DemoService demoService = (DemoService) context.getBean("demoService");
+        String hello = demoService.sayHello("zk consumer");
+        System.out.println(hello);
     }
 }
