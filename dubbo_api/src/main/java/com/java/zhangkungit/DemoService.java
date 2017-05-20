1cf83f83ed38afe13ab6f025e074e9a552f1ee4d
zhangkun <245101751@qq.com>
2017-05-20 22:31:38 +0800

    dubbo

---
+++ dubbo_api/src/main/java/com/java/zhangkungit/DemoService.java
@@ -0,0 +1,8 @@
+package com.java.zhangkungit;
+
+/**
+ * Created by zk on 2017/5/20.
+ */
+public interface DemoService {
+    public String sayHello(String name);
+}
---
+++ dubbo_consumer/src/main/java/com/java/zhangkungit/Consumer.java
@@ -0,0 +1,17 @@
+package com.java.zhangkungit;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by zk on 2017/5/20.
+ */
+public class Consumer {
+
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"dubbo-demo-consumer.xml"});
+        context.start();
+        DemoService demoService = (DemoService)context.getBean("demoService"); // get service invocation proxy
+        String hello = demoService.sayHello("world");
+        System.out.println( hello );
+    }
+}
